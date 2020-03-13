16d76e0b82d2b9d8f9707b2c6547e169e5345f83
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-13 04:34:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,9 @@ birthday_kids = {
 def happy_birthday(birthday_kids)
 
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+    if age <= 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
   end
 
 end

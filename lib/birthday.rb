48f4c8c25041f52033369d72d96b969d16bfb23b
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-13 04:40:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -18,7 +18,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    elseleanr s
+    else
       puts "You are too old for this."
     end
   end
